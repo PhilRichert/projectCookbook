ecd6f8cb5d34c25d7881832fbfbe715875dc32c3
Fennec92 <nazar.hamidulla@posteo.de>
2022-06-10 02:04:18 +0200

    updated js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const shareButton = document.querySelectorAll("#share");
 
-for (let i = 0; i < shareButton.length; i++) {
-    shareButton[i].addEventListener("click", ()=>{
+shareButton.forEach(e=>{
+    e.addEventListener("click", ()=>{
         alert("OK!");
     })
-    
-}
+})
